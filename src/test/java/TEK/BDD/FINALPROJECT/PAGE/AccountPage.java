c9a3dcdf11c61b05b46fca76ca22c7b738f0fb42
Nabila Rohani <nabilarohani786@gmail.com>
2024-05-02 18:06:03 -0700

    final

--- src/test/java/TEK/BDD/FINALPROJECT/PAGE/AccountPage.java
+++ src/test/java/TEK/BDD/FINALPROJECT/PAGE/AccountPage.java
@@ -8,5 +8,5 @@ public class AccountPage {
     public static final By AllRows= By.xpath("//tbody/tr[@class='css-0']");
     public static final By PageShow = By.xpath("//select[@class='chakra-select css-161pkch']");
     public static final By PrimaryAccountTitle = By.xpath("//*[@id='root']/div/div[3]/div/div/div/h2");
-
+//final project
 }
